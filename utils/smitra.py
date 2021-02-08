198bb32cb7a68c2dd27db46116ea366db0609b6a
mitrasudipto3 <45453764+mitrasudipto3@users.noreply.github.com>
2021-02-08 17:33:44 +0000

    change normalize to using a df (wide) input

--- utils/smitra.py
+++ utils/smitra.py
@@ -2,6 +2,9 @@ from utils.data import get_full_path
 import matplotlib.pyplot as plt
 import scipy
 import numpy as np
+from datetime import datetime
+
+reference_date = datetime(2017, 10, 1)  # index value considered 100 as reference on this date
 
 
 def sm_data_path():
@@ -24,23 +27,39 @@ def plot(s):
     plt.clf()
 
 
-def normalize(s, ub=None, lb=None):
+def normalize(df, ub=None, lb=None):
     """
-    pass a series (of positive numbers only) and it will normalize from 0 to 1 such that they sum up to 1.
+    pass a wide frame (of positive numbers only) and it will normalize from 0 to 1 such that they sum up to 1 on a date.
+    date is level 0 (row), cols are coins (level 1)
     ub and lb are upper and lower bounds on the final returned numbers - achieved by recursion
     """
-    assert s.min() > 0
-    s = s / s.sum()
+    df = df.div(df.sum(axis=1), axis=0)
     if ub is not None:
-        if s.max() > ub:
+        if df.max().max() > ub: # note: df.max() is a series
             # any number above ub gets capped at ub
             # capping at ub-0.005 to give some room of optimisation else recursion is called too many times due to
             # stringency
             # passing a_min since we have to. It is set at 0 to do nothing
-            return normalize(np.clip(s, a_max=ub-0.005, a_min=0),ub=ub,lb=lb)
+            return normalize(df.clip(lower=0,upper=ub-0.05),ub=ub,lb=lb)
     if lb is not None:
-        if s.min() < lb:
+        if df.min().min() < lb: # note: df.min() is a series
             # any number below lb gets converted to 0
-            return normalize(s.mask(s<lb,np.nan),ub=ub,lb=lb)
-    # final returned series must not have nan weights. zero weight is okay
-    return s.replace(np.nan,0)
+            return normalize(df.mask(df<lb,np.nan),ub=ub,lb=lb)
+    # final returned frame must not have nan weights. zero weight is okay
+    return df.replace(np.nan,0)
+
+def create_pivot(df, col1='date', col2='target'):
+    """
+    df must have 3 cols exactly
+    """
+    cols = list(df.columns)
+    cols.remove(col1)
+    cols.remove(col2)
+    df = df.pivot(index=col1, columns=col2, values=cols[0])
+    # drop dates with all nan
+    df = df.dropna(axis=0,how='all')
+    # drop coins with all nan
+    df = df.dropna(axis=1,how='all')
+    # keep only after reference date
+    df = df[df.index>=reference_date]
+    return df.sort_index()
